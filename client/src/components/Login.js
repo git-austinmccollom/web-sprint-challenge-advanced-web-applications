9575f89e411bb28a9caf4f56d85162b8b43ee1db
git-austinmccollom <austinmccollom@gmail.com>
2020-09-18 11:06:29 -0500

    logout button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const initialValue = {
   username: "",
@@ -9,16 +10,17 @@ const initialValue = {
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [ credentials, setCredentials ] = useState(initialValue);
-  const [ error, setError ] = useState("");
+  const [credentials, setCredentials] = useState(initialValue);
+  const [error, setError] = useState("");
+  const history = useHistory();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCredentials({
       ...credentials,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,8 +29,8 @@ const Login = () => {
       .then((res) => {
         console.log(res);
         console.log(res.data.payload);
-        localStorage.setItem('token', res.data.payload);
-        
+        localStorage.setItem("token", res.data.payload);
+        history.push("/bubbles");
       })
       .catch((err) => {
         console.error(err);
@@ -39,20 +41,21 @@ const Login = () => {
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
       <form onSubmit={handleSubmit}>
-        <label>Username 
+        <label>
+          Username
           <input
-            type='text'
-            name='username'
+            type="text"
+            name="username"
             value={credentials.username}
             onChange={handleChange}
           />
         </label>
-        <label>Password
+        <label>
+          Password
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={credentials.password}
             onChange={handleChange}
           />
